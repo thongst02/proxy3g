7fe4775ebb96377190068141bfd2b1a463c926f2
thongst02 <48155123+thongst02@users.noreply.github.com>
2020-12-28 23:32:48 +0700

    Add files via upload

--- ShellHelper/Model/GsmInfo.cs
+++ ShellHelper/Model/GsmInfo.cs
@@ -1,1 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
+namespace ShellHelper.Model
+{
+    public class GsmInfo
+    {
+        public string model { get; set; }
+        public string signal{ get; set; }
+        public string manufacturer { get; set; }
+        public string serial_number { get; set; }
+        public int operation_id { get; set; }
+        public string operation_name { get; set; }
+        public string phone_number { get; set; }
+    }
+}
---
+++ ShellHelper/Model/ModemInfo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+namespace ShellHelper.Model
+{
+    public class ModemInfo
+    {
+        //public string model { get; set; }
+        //public string operation_name { get; set; }
+        //public string serial_number { get; set; }
+        public int device_id { get; set; }
+        public GsmInfo gsmInfo { get; set; }
+        //[JsonIgnore]
+        public string path_dev { get; set; }
+        //[JsonIgnore]
+        public string name_dev { get; set; }
+        public string proxy_address { get; set; }
+        public string ip_address { get; set; }
+        [JsonIgnore]
+        public string name { get; set; }
+        //[JsonIgnore]
+        public string bus { get; set; }
+        public long time { get; set; }
+        public int status { get; set; }
+        [JsonIgnore]
+        public int procesId { get; set; }
+        [JsonIgnore]
+        public int pppdId { get; set; }
+    }
+}
